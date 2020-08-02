d5e52e4394bc38a21ca2f8eb9447e06b784a693a
Akash Sahu <57960764+akashkumarsahu987@users.noreply.github.com>
2020-08-02 22:40:27 +0530

    Add files via upload

---
+++ assignment1/src/assignment1/Palindrome.java
@@ -0,0 +1,20 @@
+package assignment1;
+import java.util.*;
+public class Palindrome{
+	
+		public static void main(String[] args) {
+			System.out.println("Enter the number to find its palindrome or not");
+			Scanner ob=new Scanner(System.in);
+			int s=ob.nextInt();
+			int j=0,n=s;
+			while(n>0)
+			{
+				int rev=n%10;
+				
+			}
+				
+				
+			
+		}
+
+}
---
+++ assignment1/src/assignment1/Reverse.java
@@ -0,0 +1,20 @@
+package assignment1;
+import java.util.Scanner;
+class Reverse
+{
+	public static void main(String args[])
+	{
+	int m,res=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the number");
+	int n=sc.nextInt();
+	while(n!=0)
+	{
+	m=n%10;
+	res=res*10+m;
+	n=n/10;
+	}
+	System.out.println("number is:"+res);
+}
+}
+
