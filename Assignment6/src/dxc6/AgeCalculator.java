9ce313e292feed75ddd5c924e31cb3d4b5430c68
Akash Sahu <57960764+akashkumarsahu987@users.noreply.github.com>
2020-08-02 22:48:04 +0530

    Add files via upload

---
+++ Assignment6/src/dxc6/AgeCalculator.java
@@ -0,0 +1,14 @@
+package dxc6;
+
+public class AgeCalculator {
+	public static int calculateAge(String st1)
+	{
+		int Age=0;
+		String st1=Integer.parseInt(st1);
+		
+		
+		
+		
+	}
+
+}
---
+++ Assignment6/src/dxc6/package-info.java
@@ -0,0 +1,1 @@
+package dxc6;
